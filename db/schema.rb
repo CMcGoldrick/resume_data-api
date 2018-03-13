a06adb56b9af41557218fbf0ef5ba873c3b2d232
matt6390 <matthewstone639@gmail.com>
2018-03-13 18:10:35 -0500

    Pulling the finished code from Josh

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180313022101) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,17 +24,6 @@ ActiveRecord::Schema.define(version: 20180313022101) do
     t.integer "student_id"
   end
 
-  create_table "experiences", force: :cascade do |t|
-    t.string "start_date"
-    t.string "end_date"
-    t.string "job_title"
-    t.string "company_name"
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "student_id"
-  end
-  
   create_table "educations", force: :cascade do |t|
     t.integer "student_id"
     t.string "start_date"
@@ -46,6 +33,17 @@ ActiveRecord::Schema.define(version: 20180313022101) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.string "job_title"
+    t.string "company_name"
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "students", force: :cascade do |t|
