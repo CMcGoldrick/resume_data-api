d2cde160f8f4c7cf2b1dcbc1637153face593f6d
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-03-12 20:41:07 -0500

    created Student controller, view and CRUD routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/students' => 'students#index'
+  get '/students/:id' => 'students#show'
+
+  post '/students' => 'students#create'
+  patch '/students/:id' => 'students#update'
+  destroy '/students/:id' => 'students#destroy'
 
 end
