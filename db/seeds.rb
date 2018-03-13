cce87ba5c7caa139c16b4d59ec32733304a829ee
matt6390 <matthewstone639@gmail.com>
2018-03-12 21:26:54 -0500

    Dumped Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Capstone.create!([
+  {name: "Card Capstone", description: "By far the greatest capstone ever seen", url: "google.com/thisisnotarealurl", student_id: 1}
+])
+Experience.create!([
+  {start_date: "3/12/17", end_date: "3/12/18", job_title: "hard worker", company_name: "That one company", details: "You know, I did some stuff, and saw some things.", student_id: 1}
+])
 Student.create!([
   {first_name: "Matthew", last_name: "Stone", email: nil, phone_number: nil, short_bio: nil, twitter_handle: nil, linkedin_url: nil, personal_website_url: nil, online_resume_url: nil, github_url: nil}
 ])
