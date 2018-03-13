0f3fc5d5963839dc471510a91304afa3191cf4c2
Teleportal <joshthegreenberg@gmail.com>
2018-03-13 18:18:14 -0500

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ Education.create!([
   {student_id: 1, start_date: "9/15/14", end_date: "5/15/16", degree: "BS in Biology", university: "University of Illinois", details: "Studied a variety of subjects with a variety of professors."}
 ])
 Student.create!([
-  {first_name: "Matthew", last_name: "Stone", email: "mattstone@gmail.com", phone_number: "1-630-555-6789", short_bio: "He's a nice guy", twitter_handle: "@mattstone", linkedin_url: "linkedin.com/mattstone", personal_website_url: "mattstone.com", online_resume_url: nil, github_url: nil}
+  {first_name: "Matthew", last_name: "Stone", email: nil, phone_number: nil, short_bio: nil, twitter_handle: nil, linkedin_url: nil, personal_website_url: nil, online_resume_url: nil, github_url: nil}
 ])
