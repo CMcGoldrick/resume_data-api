c584eb42a9a0f329f94a4358dff5cafff6a5eb84
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-03-12 19:46:49 -0500

    created Student model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.create!([
+  {first_name: "Matthew", last_name: "Stone", email: nil, phone_number: nil, short_bio: nil, twitter_handle: nil, linkedin_url: nil, personal_website_url: nil, online_resume_url: nil, github_url: nil}
+])
+
