d7ba7d1754dbf3d9b4bb8fde9341962bdd77c918
matt6390 <matthewstone639@gmail.com>
2018-03-13 18:25:50 -0500

    About to update routes and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,5 @@ Experience.create!([
 ])
 
 Education.create(student_id: 1, start_date: "9/15/16", end_date: "5/15/17", degree: "BA in Philosophy", university: "University of Illinois at Chicago", details: "Studied a variety of subjects with a variety of professors.")
+
 Education.create(student_id: 1, start_date: "9/15/14", end_date: "5/15/16", degree: "BS in Biology", university: "University of Illinois", details: "Studied a variety of subjects with a variety of professors.")
