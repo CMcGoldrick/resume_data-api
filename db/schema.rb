862e3ce698ad91e4920016b86cf83eecf7569e57
Teleportal <joshthegreenberg@gmail.com>
2018-03-12 21:20:48 -0500

    skills added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313011447) do
+ActiveRecord::Schema.define(version: 20180313021710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180313011447) do
     t.string "degree"
     t.string "university"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.integer "student_id"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# Student.create!([
-#   {first_name: "Matthew", last_name: "Stone", email: "mattstone@gmail.com", phone_number: "1-630-555-6789", short_bio: "He's a nice guy", twitter_handle: "@mattstone", linkedin_url: "linkedin.com/mattstone", personal_website_url: "mattstone.com", online_resume_url: nil, github_url: nil}
-# ])
-
-Education.create(student_id: 1, start_date: "9/15/16", end_date: "5/15/17", degree: "BA in Philosophy", university: "University of Illinois at Chicago", details: "Studied a variety of subjects with a variety of professors.")
-Education.create(student_id: 1, start_date: "9/15/14", end_date: "5/15/16", degree: "BS in Biology", university: "University of Illinois", details: "Studied a variety of subjects with a variety of professors.")
-
+Education.create!([
+  {student_id: 1, start_date: "9/15/16", end_date: "5/15/17", degree: "BA in Philosophy", university: "University of Illinois at Chicago", details: "Studied a variety of subjects with a variety of professors."},
+  {student_id: 1, start_date: "9/15/14", end_date: "5/15/16", degree: "BS in Biology", university: "University of Illinois", details: "Studied a variety of subjects with a variety of professors."}
+])
+Student.create!([
+  {first_name: "Matthew", last_name: "Stone", email: "mattstone@gmail.com", phone_number: "1-630-555-6789", short_bio: "He's a nice guy", twitter_handle: "@mattstone", linkedin_url: "linkedin.com/mattstone", personal_website_url: "mattstone.com", online_resume_url: nil, github_url: nil}
+])
